4d01040e3070aa52062b8fdf3d436d163dddb706
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-05-29 11:17:21 +0200

    Add addBook function

--- projects/library/script.js
+++ projects/library/script.js
@@ -1,33 +1,37 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = function () {
-    return read ? "has been read" : "not read yet";
-  };
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${
-      this.pages
-    } pages, ${this.read()}.`;
-  };
+  this.read = read;
 }
 
-const book1 = new Book(
-  "Influence: The Psychology of Persuasion",
-  "R. Cialdini",
-  295,
-  true
-);
+Book.prototype.read = function () {
+  return read ? "has been read" : "not read yet";
+};
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${
+    this.pages
+  } pages, ${this.read()}.`;
+};
 
-const book2 = new Book("A Game of Thrones", "George R. R. Martin", 694, false);
+function addBook() {
+  const title = prompt("TITLE");
+  const author = prompt("AUTHOR");
+  const pages = prompt("PAGES");
+  const read = prompt("READ?");
+  const checkRead = read.toLowerCase() === "yes";
+  const book = new Book(title, author, pages, checkRead);
+  myLibrary.push(book);
+}
 
-const book3 = new Book("Sword of Destiny", "A. Sapkowski", 384, false);
+addBook();
+console.log(myLibrary);
 
-const book4 = new Book(
-  "Thinking, Fast and Slow",
-  "Daniel Kahneman",
-  499,
-  false
-);
-
-console.log(book1.info());
+// const book = new Book(
+//   "Influence: The Psychology of Persuasion",
+//   "R. Cialdini",
+//   295,
+//   true
+// );
