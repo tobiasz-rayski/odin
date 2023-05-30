d73d6d51bf48b3c23cdaedee04e37dd50a947375
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-05-30 10:03:05 +0200

    Remove card functionality added both for DOM and array

--- projects/library/script.js
+++ projects/library/script.js
@@ -1,17 +1,19 @@
 let myLibrary = [];
+let idCounter = 0;
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, idNum) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.idNum = idNum;
 }
 
-const wrapper = document.getElementById("wrapper");
 const cardContainer = document.createElement("div");
-const modal = document.getElementById("modal");
 const buttonContainer = document.createElement("div");
 const buttonOpenModal = document.createElement("button");
+const wrapper = document.getElementById("wrapper");
+const modal = document.getElementById("modal");
 buttonOpenModal.textContent = "New Book";
 const buttonCloseModal = document.getElementById("close-modal");
 const addButton = document.getElementById("addButton");
@@ -44,66 +46,64 @@ buttonCloseModal.addEventListener("click", (event) => {
   }
 });
 
-addButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const titleForm = document.getElementById("book-title").value;
-  const authorForm = document.getElementById("book-author").value;
-  const pagesForm = document.getElementById("book-pages").value;
-  const readForm =
-    document.querySelector(`input[name="book_read"]:checked`).value === "true";
-  const book = new Book(titleForm, authorForm, pagesForm, readForm);
-  myLibrary.push(book);
-  addBookToLibrary(book);
-  closeModal();
-});
-
 window.addEventListener("keydown", (event) => {
   if (event.keyCode === 27) {
     closeModal();
   }
 });
 
-function addBookToLibrary(item) {
-  const card = document.createElement("div");
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const title = document.getElementById("book-title").value;
+  const author = document.getElementById("book-author").value;
+  const pages = document.getElementById("book-pages").value;
+  const read =
+    document.querySelector(`input[name="book_read"]:checked`).value === "true";
+  const idNum = idCounter;
+  idCounter++;
+  const book = new Book(title, author, pages, read, idNum);
+  myLibrary.push(book);
+
+  addBookToLibrary(book);
+  closeModal();
+});
+
+function addBookToLibrary(book) {
+  let card = document.createElement("div");
   const title = document.createElement("p");
   const author = document.createElement("p");
   const pages = document.createElement("p");
   const read = document.createElement("p");
   const readButton = document.createElement("div");
+  const deleteCard = document.createElement("span");
 
-  title.textContent = item.title;
-  author.textContent = item.author;
-  pages.textContent = item.pages + " pages";
-  read.textContent = item.read ? "Read" : "Not read yet";
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages + " pages";
+  read.textContent = book.read ? "Read" : "Not read yet";
+  deleteCard.innerHTML = "&times";
+
+  card.setAttribute("data-id", book.idNum);
 
   card.appendChild(title);
   card.appendChild(author);
   card.appendChild(pages);
   card.appendChild(read);
   card.appendChild(readButton);
+  card.appendChild(deleteCard);
 
   card.classList.add("card");
   title.classList.add("title");
   author.classList.add("author");
   pages.classList.add("pages");
   read.classList.add("read");
-  item.read
-    ? readButton.classList.add("read-button-on")
-    : readButton.classList.add("read-button-off");
-
-  if (!item.read) {
-    read.classList.add("text-red");
-  }
-
+  deleteCard.classList.add("delete-card");
   cardContainer.appendChild(card);
 
-  readButton.addEventListener("click", function () {
-    if (readButton.classList.contains("read-button-on")) {
-      readButton.classList.remove("read-button-on");
-      readButton.classList.add("read-button-off");
-    } else {
-      readButton.classList.remove("read-button-off");
-      readButton.classList.add("read-button-on");
-    }
+  deleteCard.addEventListener("click", (event) => {
+    const parent = event.target.parentNode;
+    const dataId = Number(parent.getAttribute("data-id"));
+    parent.remove();
+    myLibrary = myLibrary.filter((book) => book.idNum !== dataId);
   });
 }
