2c39f350bbdc4ba20bea3c891e9cb795ce3e701d
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-06-05 12:25:44 +0200

    Add reset board function Add placeMark function Add createPlayer function Add changeTurns function

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 const gameBoard = (function () {
-  const gameBoard = new Array(9).fill(null);
-  let mark = "x";
+  const board = new Array(9).fill(null);
 
-  const randomFill = () => {
-    while (gameBoard.some((item) => item === null)) {
-      let random = Math.floor(Math.random() * 9);
-      if (gameBoard[random] === null) {
-        gameBoard[random] = mark;
-        mark = mark === "x" ? "o" : "x";
-      }
-    }
-  };
-
-  const isWin = () => {
+  const isWin = (mark) => {
     const winConditions = [
       [0, 1, 2],
       [3, 4, 5],
@@ -23,10 +12,70 @@ const gameBoard = (function () {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
+    let win = winConditions.some((arr) =>
+      arr.every((index) => board.at(index) === mark)
+    );
+
+    if (win) {
+      console.log(`${mark} wins.`);
+      return true;
+    }
+    return false;
   };
 
-  return { gameBoard, randomFill, isWin };
+  const reset = () => {
+    board.fill(null);
+  };
+
+  const changeTurns = () => {
+    player1.isTurn = !player1.isTurn;
+    player2.isTurn = !player2.isTurn;
+  };
+
+  const placeMark = (player, space) => {
+    if (board[space] === null) {
+      board[space] = player.mark;
+      isWin(player.mark);
+    }
+  };
+
+  return {
+    board,
+    isWin,
+    reset,
+    placeMark,
+    changeTurns,
+  };
 })();
 
-gameBoard.randomFill();
-console.log(gameBoard.gameBoard);
+const Player = (name, mark) => {
+  const isTurn = false;
+
+  return {
+    name,
+    mark,
+    isTurn,
+  };
+};
+
+const player1 = Player("Jeff", "x");
+const player2 = Player("Tom", "o");
+player1.isTurn = true;
+
+// gameBoard.placeMark(player1, 0);
+// console.log(gameBoard.board);
+
+// gameBoard.reset();
+
+// const randomFill = () => {
+//   let mark = "x";
+//   while (board.some((item) => item === null)) {
+//     let random = Math.floor(Math.random() * 9);
+//     if (board[random] === null) {
+//       board[random] = mark;
+//       if (isWin(mark) === true) break;
+//       mark = mark === "x" ? "o" : "x";
+//     }
+//   }
+// };
